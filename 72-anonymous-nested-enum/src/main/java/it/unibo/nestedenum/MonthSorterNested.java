60e82195bd72d404b25c376afd54223bc0ae003d
2Skali3 <130842781+2Skali3@users.noreply.github.com>
2023-11-07 12:05:29 +0100

    lab-72 P3 creato enum innestato Month e creato campo che modella il numero di giorni del mese

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -18,4 +18,25 @@ public final class MonthSorterNested implements MonthSorter {
     public Comparator<String> sortByOrder() {
         return null;
     }
+
+    private static enum Month{
+        GENNAIO(31),
+        FEBBRAIO(28),
+        MARZO(31),
+        APRILE(30),
+        MAGGIO(31),
+        GIUGNO(30),
+        LUGLIO(31),
+        AGOSTO(31),
+        SETTEMBRE(30),
+        OTTOBRE(31),
+        NOVEMBRE(30),
+        DICEMBRE(31);
+
+        private final int giorni;
+        
+        Month(final int gg){
+            this.giorni = gg;
+        }
+    }
 }
