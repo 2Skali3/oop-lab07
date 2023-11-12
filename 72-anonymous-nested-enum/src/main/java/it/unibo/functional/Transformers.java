171ed681be33a2ae30e3b8310c03558fd25ff32d
2Skali3 <130842781+2Skali3@users.noreply.github.com>
2023-11-12 23:28:04 +0100

    Finito lab-72 completato i metodi di select,reject,flatten di P2; completato metodo fromString,sortByOrder,sortByDays di P3

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -4,6 +4,7 @@ import it.unibo.functional.api.Function;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -42,7 +43,7 @@ public final class Transformers {
     /**
      * A function that applies a transformation to each element of an {@link Iterable},
      * returning a list of these transformed elements.
-     * For instance, {@code [1, 2, 3, 4, 5]} could use {@code transform} to produce a list of squares by passing
+     * For instance, {@code [1, 2, 3, 4, 5]} could use {@code transform} to p roduce a list of squares by passing
      * a function that computes the square of the input, thus obtaining {@code [1, 4, 9, 16, 25]}.
      * <b>NOTE:</b> this function is a special flattenTransform whose function always return a list with a single
      * element
@@ -54,14 +55,21 @@ public final class Transformers {
      * @param <O> output elements type
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends O>>()
+            {
+                @Override
+                public Collection<? extends O> call(I input) {
+                    return List.of(transformer.call(input));
+                }
+            }
+        );
     }
 
     /**
      * A function that takes an iterable of collections, and returns a flatten list of the elements of the inner
      * collections.
-     * For instance, {@code [[1], [2, 3], [4, 5], []]} could use {@code flatten} to produce a flat list, thus obtaining
-     * {@code [1, 2, 3, 4, 5]}.
+     * For instance, {@code [[1], [2, 3], [5, 5], []]} could use {@code flatten} to produce a flat list, thus obtaining
+     * {@code [1, 2, 3, 5, 5]}.
      * <b>NOTE:</b> this function is a special flattenTransform whose input is an iterable of collections,
      * and whose function simply returns each collection (identity).
      *
@@ -70,7 +78,7 @@ public final class Transformers {
      * @param <I> type of the collection elements
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -87,7 +95,15 @@ public final class Transformers {
      * @param <I> elements type
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return flattenTransform(base, new Function<I,Collection<? extends I>>() {
+            @Override
+                public Collection<? extends I> call(I input) {
+                    if (test.call(input)){
+                        return List.of(input);
+                    }
+                    return Collections.emptyList();
+                }
+        });
     }
 
     /**
@@ -103,6 +119,12 @@ public final class Transformers {
      * @param <I> elements type
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        return select(base, new Function<I,Boolean>() {
+
+            @Override
+            public Boolean call(I input) {
+                return !test.call(input);
+            }  
+        });
     }
 }
--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,6 +1,8 @@
 package it.unibo.nestedenum;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -11,32 +13,65 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(final String o1, final String o2) {
+                return Month.fromString(o1).giorni - Month.fromString(o2).giorni;
+            }
+            
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(final String o1, final String o2) {
+                return Month.fromString(o1).ordinal() - Month.fromString(o2).ordinal();
+            }
+            
+        };
     }
 
     private static enum Month{
-        GENNAIO(31),
-        FEBBRAIO(28),
-        MARZO(31),
-        APRILE(30),
-        MAGGIO(31),
-        GIUGNO(30),
-        LUGLIO(31),
-        AGOSTO(31),
-        SETTEMBRE(30),
-        OTTOBRE(31),
-        NOVEMBRE(30),
-        DICEMBRE(31);
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
 
         private final int giorni;
         
         Month(final int gg){
-            this.giorni = gg;
+            this.giorni = 
+            gg;
         }
+
+        private static Month fromString(String text){
+            text = text.toUpperCase();
+            List<Month> matching = new ArrayList<>();
+            for (Month m : Month.values()) {
+                if (m.toString().startsWith(text)) {
+                    matching.add(m);
+                }
+            }
+            if (matching.size() == 0) {
+                throw new IllegalArgumentException("string provided does not match any month");
+            } else if (matching.size() > 1) {
+                throw new IllegalArgumentException("ambiguous string provided");
+            } else {
+                return matching.get(0);
+            }
+        }
+
     }
 }
